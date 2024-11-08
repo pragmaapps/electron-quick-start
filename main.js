0cff5492903f9a6d9055a78b182b8e0ed28d5e59
vivek.gakhar <vivek.gakhar@pragmaapps.com>
2024-11-08 08:46:54 +0200

    added roundedCorners property

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ function createWindow () {
     width: 800,
     height: 600,
     frame: false,
+    roundedCorners: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
